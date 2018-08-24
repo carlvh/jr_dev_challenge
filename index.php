9f9aa9b426b3c12d0e6383d7524f7d60ab4bc382
Carl A. Fahlstrom <carl@viralhog.com>
2018-08-24 10:22:29 -0600

    initial commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<html>
+  <head>
+  </head>
+
+  <body>
+
+    <style>
+
+    </style>
+
+    <!-- Make inputs for name, address, email -->
+
+    <button id="submitButton">Submit</button>
+
+    <table id="userTable">
+      <tr>
+        <!-- Make a table header - Include Name, Address, email -->
+      </tr>
+    	<!-- Make a php code block here that fetchs from db and adds a row for each db result, only select the first 100 results -->
+      <?php
+
+      ?>
+    </table>
+
+    <!-- Make a php array, loop to output as a ul html list -->
+    <?php
+
+    ?>
+    <script>
+      // How do you make a javascipt closure?  What are the benifits.
+
+      // Use JQuery to send the data to the database with an AJAX call.  Add the row to the table dynamically.
+
+      // How do you wait for the page to load before running a function
+
+      // Attach an event handler function to the button click.
+
+      // Make the handler add a new row to the db by AJAX request.
+
+      // Use JQuery to add the row without a page reload.
+
+    </script>
+  </body>
+</html>
---
+++ user_class.php
@@ -0,0 +1,53 @@
+<?php
+
+// In SQL create a user table with feilds for id, name, email, group_id, address_id
+
+// Create a group table and address tables include keys to the user table so that join select statements can be done.
+
+// If there are PHP keywords here that you don't understand, Google it.
+
+// `` are column names
+// '' are values
+
+function fill_user_list_variables($args) {
+	extract($args);  // What does extract do?
+
+  $where_sql = "WHERE 1";
+  $sort_sql = "";
+  $from_sql = "";
+  $search_sql = "";
+  $param_array = array();
+
+  // assume that this sets up the database correctly.
+  $db = new PDO("config string");
+  // $group, $order, $dir, $q - these are the optional variable that may be passed in.
+
+  //select all rows and columns form a table called users.
+
+  // select the `name` of all the users in 'guest' `group` if a group variable to be passed to func and set to guest. Assume a $group variable passed in.
+  // Place the selection into a variable $select_sql.  Use conditionals and add to the $where_sql variable.
+
+  // sort the results by `number_of_files` largest to smallest, make it possible to sort smallest to largest.  Assume a $order and $dir variable - put in $sort_sql
+
+  // Do a text search for a partial name - put in $search_sql
+
+  // combine partial sql stmts into $sql_stmt_st
+  $sql_stmt_st =
+
+  // make the statment - this prepares and executes the statement.
+  $sql_stmt = $db->prepare($sql_stmt_st);
+  $sql_stmt->execute($param_array);
+
+  $result = $sql_stmt->fetch(); // Use in a loop - this returns one result at a time until
+
+  // return an array of associtive arrays of all users.
+}
+
+
+function add_user($args){
+  extract($args);
+	// Insert a row into the user table.  Data -> username, Full Name, email, group id
+  return json_ecncode(array('error'=>0, 'msg'=>''));
+}
+
+ ?>
